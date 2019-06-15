91492c4346abd3c7aa54188f379c41a83ea67408
buonzz <buonzz@gmail.com>
2019-06-16 00:06:28 +0800

    call api inside blade template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,11 +49,8 @@
     <div class="container d-flex justify-content-between">
       <a href="#" class="navbar-brand d-flex align-items-center">
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="mr-2" viewBox="0 0 24 24" focusable="false"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg>
-        <strong>Album</strong>
+        <strong>A Horribly Written Laravel App</strong>
       </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
     </div>
   </div>
 </header>
@@ -61,6 +58,12 @@
 <main role="main">
   <div class="album py-5 bg-light">
     <div class="container">
+
+      @php
+        $data = file_get_contents("https://picsum.photos/v2/list");
+        dd($data);
+      @endphp
+
 
       <div class="row">
         <div class="col-md-4">
