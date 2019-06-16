7721302598e8e78572e8a2e57bb49e120a41edfa
buonzz <buonzz@gmail.com>
2019-06-16 08:50:09 +0800

    added field for votes

--- storage/db.sql
+++ storage/db.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS `Pictures` (
   `photo_id` int NOT NULL default 0,
   `width` int NOT NULL default 0,
   `height` int NOT NULL default 0,
+  `votes` int NOT NULL default 0,
   `author` varchar(100) NOT NULL default 'None',
-  `description` varchar(255)  default 'None',
+  `description` varchar(255)  default 'None'
    PRIMARY KEY  (`id`)
 );
