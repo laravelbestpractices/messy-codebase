33b8074fa4891a203edccedcbbefcc1f4ca94fad
buonzz <buonzz@gmail.com>
2019-06-16 23:14:39 +0800

    updating value in mysql db

--- public/track.php
+++ public/track.php
@@ -5,21 +5,34 @@ $dotenv = Dotenv\Dotenv::create('../');
 $dotenv->load();
 
 $vote_method = isset($_GET['vote_method']) ? $_GET['vote_method'] : 'up';
+$photoID = isset($_GET['photoID']) ? $_GET['photoID'] : 0;
 
 
-$host =  getenv('DB_HOST');
-$db   = getenv('DB_DATABASE');
-$user = getenv('DB_USERNAME');
-$pass = getenv('DB_PASSWORD');
+if($photoID > 0)
+{
+    $host =  getenv('DB_HOST');
+    $db   = getenv('DB_DATABASE');
+    $user = getenv('DB_USERNAME');
+    $pass = getenv('DB_PASSWORD');
+    
+    $dsn = "mysql:host=$host;dbname=$db";
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    try {
+        
+        $pdo = new PDO($dsn, $user, $pass, $options);
+        
+        if($vote_method == 'up')
+            $sql = "UPDATE Pictures SET Votes = Votes + 1 WHERE picID = '$photoID'";
+         else
+            $sql = "UPDATE Pictures SET Votes = Votes - 1 WHERE picID = '$photoID'";
 
-$dsn = "mysql:host=$host;dbname=$db";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        $res = $pdo->exec($sql);
+        var_dump($res);
+    } catch (\PDOException $e) {
+         throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
 }
