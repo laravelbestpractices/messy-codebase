7cda7496c8e1208c50ba40de3fcba6124ea6bbf6
buonzz <buonzz@gmail.com>
2019-07-13 23:28:26 +0800

    log to stderr

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -16,8 +16,7 @@ class ExampleTest extends DuskTestCase
     public function testBasicExample()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/')->dump();
-                   // ->assertSee('Laravel App');
+            $browser->visit('/')->assertSee('Laravel App');
         });
     }
 }
