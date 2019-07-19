316c7f6f070d10c906dc5baff69861f71672b5e1
buonzz <buonzz@gmail.com>
2019-07-19 22:54:33 +0800

    added a horrible way to check for images

--- app/functions.php
+++ app/functions.php
@@ -22,4 +22,13 @@ function checkIFPhotoExists($photoid){
         return false;
 }
 
-?>
+function getHttpCode($http_response_header)
+{
+    if(is_array($http_response_header))
+    {
+        $parts=explode(' ',$http_response_header[0]);
+        if(count($parts)>1) //HTTP/1.0 <code> <text>
+            return intval($parts[1]); //Get code
+    }
+    return 0;
+}
